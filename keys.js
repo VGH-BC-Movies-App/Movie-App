44bfd16eb6383e4ebe4bbfed74fb8b9fe3ea17ef
Victor G Hernandez <victor.g.hernandez87@gmail.com>
2021-04-20 15:34:33 -0500

    sec commit

---
+++ keys.js
@@ -0,0 +1,1 @@
+const movieKey = "http://www.omdbapi.com/?i=tt3896198&apikey=7ed1ce70"
--- movie.js
+++ movie.js
@@ -38,3 +38,82 @@ let movieObj = {"title": "resident evil",
 fetch(putURL, options).then(function(response){
     console.log(response)
 })
+
+
+// movieKey
+
+fetch(`${movieKey}`)
+    //  Parse the response into json
+    .then((response)=>{return response.json()})
+    // access the results property from the json Object
+    .then((jsonData)=>{
+        console.log(jsonData);
+        return jsonData.results;
+    })
+    // iterate over the results array, and access title and id properties of each film
+    .then((results)=>{
+        results.forEach((film)=>console.log(`${film.title} ${film.episode_id}`))
+    })
+    // This runs if my fetch request fails. Try changing films to xfilms in the url and look at the error that gets passed. It isn't what you think it would be.
+    .catch(error => console.log('Error getting films.', error));
+
+// Notice how this log runs before the we log the results.
+console.log(movieKey);
+
+
+ /// adding movies to list/////
+
+
+function renderCoffee(coffee) {
+    var html = '<div class="coffee">';
+    // html += '<td>' + coffee.id + '</td>';
+    html += '<div class="d-flex align-items-baseline">';
+    html += '<h3>' + coffee.name + '</h3>';
+    html += '<p class="ml-2 text-secondary">' + coffee.roast + '</p>';
+    html += '</div>';
+    html += '</div>';
+
+    return html;
+}
+
+function renderCoffees(coffees) {
+    var html = '';
+    for(var i = 0; i < coffees.length; i++) {
+        html += renderCoffee(coffees[i]);
+    }
+    return html;
+}
+
+
+
+function updateCoffees(e) {
+    e.preventDefault(); // don't submit the form, we just want to update the data
+    var selectedRoast = roastSelection.value;
+    var coffeNameInput = coffeeName.value.toLowerCase();
+    var filteredCoffees = [];
+    coffees.forEach(function(coffee) {
+        if (coffee.roast === selectedRoast && coffee.name.toLowerCase().indexOf(coffeNameInput) !== -1) {
+            filteredCoffees.push(coffee);
+        }else if(selectedRoast === "All" && coffee.name.toLowerCase().indexOf(coffeNameInput) !== -1) {
+            filteredCoffees.push(coffee);
+        }
+    });
+    tbody.innerHTML = renderCoffees(filteredCoffees);
+}
+
+
+
+
+function addCoffee(e) {
+    e.preventDefault();
+    coffees.push({
+        name: coffeeAddName.value,
+        roast: roastAddSelection.value
+    });
+    var filteredCoffees = [];
+    coffees.forEach(function(coffee) {
+        filteredCoffees.push(coffee)
+    });
+    tbody.innerHTML = renderCoffees(filteredCoffees);
+
+}
